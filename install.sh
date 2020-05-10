c701cbac1441abbde53c77cb52a5c2baf1f6b8d2
indoes <40215734+indoes@users.noreply.github.com>
2020-05-09 18:13:54 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,14 @@
 
 echo >> .bashrc
 echo >> .bashrc
-curl https://raw.githubusercontent.com/indoes/bash_history_forever/master/.bashrc >> .bashrc
+curl -s -S https://raw.githubusercontent.com/indoes/bash_history_forever/master/.bashrc >> .bashrc
+echo "Appended bash_history_forever to .bashrc"
 
-curl https://raw.githubusercontent.com/indoes/bash_history_forever/master/usr/local/bin/deduplicate-bash-history.sh >> /usr/local/bin/deduplicate-bash-history.sh
+curl -s -S https://raw.githubusercontent.com/indoes/bash_history_forever/master/usr/local/bin/deduplicate-bash-history.sh >> /usr/local/bin/deduplicate-bash-history.sh
+echo "Installed /usr/local/bin/deduplicate-bash-history.sh"
 
 chmod 750 /usr/local/bin/deduplicate-bash-history.sh
+echo "Make /usr/local/bin/deduplicate-bash-history.sh executable"
 
 crontab -l | { cat; printf "\n#------------------\n# bash_hist_dedup\n#------------------\n@reboot /usr/local/bin/deduplicate-bash-history.sh\n"; } | crontab -
+echo "Add @reboot /usr/local/bin/deduplicate-bash-history.sh to users crontab"
