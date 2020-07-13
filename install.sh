84a847b9fce7e2f8ee646cff23fc19b17fd00fea
indoes <nsspam.d@gmail.com>
2020-07-13 14:04:54 -0700

    updated for apt or yum

--- install.sh
+++ install.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-echo >> .bashrc
-echo >> .bashrc
+instapt=echo "$(which apt)"
+instyum=echo "$(which yum)"
+
+if [ -x $instapt ]; then
+	apt-get update && apt -y install gawk
+fi
+if [ -x $instyum ]; then
+	yum install -y gawk
+fi
+
+echo -e "\n" >> .bashrc
+#echo >> .bashrc
+
+echo "Appending bash_history_forever to .bashrc"
 curl -s -S https://raw.githubusercontent.com/indoes/bash_history_forever/master/.bashrc >> .bashrc
-echo "Appended bash_history_forever to .bashrc"
+sleep 2
 
+echo "Installing deduplicate-bash-history.sh to /usr/local/bin/"
 curl -s -S https://raw.githubusercontent.com/indoes/bash_history_forever/master/usr/local/bin/deduplicate-bash-history.sh >> /usr/local/bin/deduplicate-bash-history.sh
-echo "Installed /usr/local/bin/deduplicate-bash-history.sh"
+sleep 2
 
-chmod 750 /usr/local/bin/deduplicate-bash-history.sh
+chmod 755 /usr/local/bin/deduplicate-bash-history.sh
 echo "Make /usr/local/bin/deduplicate-bash-history.sh executable"
 
 crontab -l | { cat; printf "\n#------------------\n# bash_hist_dedup\n#------------------\n@reboot /usr/local/bin/deduplicate-bash-history.sh\n"; } | crontab -
